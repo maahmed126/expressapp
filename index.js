32050176ae9b12325158abe0711dc06502ee4c62
Md Ahmed <maahmed126@gmail.com>
2021-12-07 18:35:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8000;
+const baseUrl = `http://localhost:${port}`;
+
+
+app.get('/', (req, res) => {
+   res.status(200).send('hello world!');
+});
+
+// Server
+app.listen(port, () => {
+   console.log(`Listening on: http://localhost:${port}`);
+});
